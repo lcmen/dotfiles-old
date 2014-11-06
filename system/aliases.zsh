2e55102fc5fabce083a6ab0f402a1f914b5a82fb
Lucas Mendelowski <lucas@mendelowski.com>
2014-11-06 07:56:27 +0100

    Updated config files and added spotify control

--- system/aliases.zsh
+++
@@ -1,9 +0,0 @@
-alias ..="cd .."
-alias cd..="cd .."
-alias e="exit"
-
-# Flush dns
-alias flushdns="dscacheutil -flushcache"
-
-# Simple HTTP server
-alias httpserver="Python -m SimpleHTTPServer"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,18 @@
+# Reload zsh config
 alias reload!='. ~/.zshrc'
+
+# Directories
+alias ..="cd .."
+alias cd..="cd .."
+alias e="exit"
+
+# Flush dns
+alias flushdns="dscacheutil -flushcache"
+
+# Simple HTTP server
+alias httpserver="Python -m SimpleHTTPServer"
+
+# Spotify Control
+alias s="osascript ~/.dotfiles/spotify-control/SpotifyControl.scpt"
+alias sn="osascript ~/.dotfiles/spotify-control/SpotifyControl.scpt next"
+alias sp="osascript ~/.dotfiles/spotify-control/SpotifyControl.scpt prev"
